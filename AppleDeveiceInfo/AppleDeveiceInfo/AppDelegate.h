270cc8d92c3112df66b0f899d5d19c9a349eeb86
Tomorrow <1500946604@qq.com>
2018-12-04 11:41:09 +0800

    初始化代码

---
+++ AppleDeveiceInfo/AppleDeveiceInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleDeveiceInfo
+//
+//  Created by Tomorrow on 2018/12/4.
+//  Copyright © 2018年 Tomorrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleDeveiceInfo/AppleDeveiceInfo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppleDeveiceInfo
+//
+//  Created by Tomorrow on 2018/12/4.
+//  Copyright © 2018年 Tomorrow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
